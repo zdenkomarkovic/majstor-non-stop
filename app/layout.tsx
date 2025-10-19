53e263c5d172ab413f0fed0e22a2adfeb6c5b0a7
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-10-20 01:29:39 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import ButtonToTop from "@/components/ButtonToTop";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import head from "@/node_modules/next/head";
-import { after } from "@/node_modules/next/server";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-import page from "./page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
