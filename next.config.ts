0eb02ad3dd96a2452d89fa2cf08f99ba43412b2f
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-10-31 03:08:13 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://pagead2.googlesyndication.com; img-src 'self' data: https:; connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
