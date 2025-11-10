59360ecc339b42e8b7b6d22ae02a04bbd1d87653
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-11-10 02:51:56 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,13 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://stats.g.doubleclick.net https://pagead2.googlesyndication.com;
-              connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net;
-              img-src 'self' data: https://www.google-analytics.com;
-              style-src 'self' 'unsafe-inline';
-              frame-src https://www.googletagmanager.com;
-            `.replace(/\s+/g, " "), // uklanja višak praznih mesta
+            default-src 'self';
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://stats.g.doubleclick.net https://pagead2.googlesyndication.com;
+            connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net;
+            img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
+            style-src 'self' 'unsafe-inline';
+            frame-src https://www.googletagmanager.com;
+          `.replace(/\s+/g, " "),
           },
         ],
       },
