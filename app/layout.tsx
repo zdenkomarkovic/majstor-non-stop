bb6e1460db1cbbce14b35f76d4849ffb6797c055
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-02-26 00:36:59 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -117,29 +117,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-muted-foreground bg-muted  text-base md:text-xl`}
       >
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WGV5ZXCW"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
         <Header />
         {children}
         <ButtonToTop />
         <Footer />
-
-        <Script id="gtm" strategy="afterInteractive">
-          {`
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-WGV5ZXCW');
-`}
-        </Script>
 
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17670700223"
