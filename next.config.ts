1968f3dfd783c3848739e46e42c214b821f10825
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-03-17 23:09:54 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -10,9 +10,9 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
             default-src 'self';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://stats.g.doubleclick.net https://pagead2.googlesyndication.com;
-            connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net;
-            img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://stats.g.doubleclick.net https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.googleadservices.com;
+            connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://googleads.g.doubleclick.net https://www.googleadservices.com https://www.google.com;
+            img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.googleadservices.com;
             style-src 'self' 'unsafe-inline';
             frame-src https://www.googletagmanager.com;
           `.replace(/\s+/g, " "),
